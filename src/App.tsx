dcc75d1f9d6e7600cb95ee438f8be8a7ec0e768a
TMK04 <floer.learner.01@gmail.com>
2022-11-03 23:44:38 +0800

    refactor(App header): wrap contact info in address el

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ function App() {
         <h1>
           Tran Minh <strong>Khoa</strong>
         </h1>
-        <div>
+        <address>
           <Link href="mailto:t.m.khoa.2308@gmail.com">t.m.khoa.2308@gmail.com</Link>
           <SeparatorInline className="fw-bold">|</SeparatorInline>
           <Link href="tel:+6580138550">+65 8013 8550</Link>
-        </div>
-        <div>
-          <Link href="mailto:2101432i@student.tp.edu.sg">2101432i@student.tp.edu.sg</Link>
-        </div>
+          <div>
+            <Link href="mailto:2101432i@student.tp.edu.sg">2101432i@student.tp.edu.sg</Link>
+          </div>
+        </address>
       </header>
       <Separator />
       <main className="mx-5">
