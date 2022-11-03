33019fc7e73d3618f1224db49ecdd6909de3d60a
TMK04 <floer.learner.01@gmail.com>
2022-11-03 22:29:04 +0800

    feat(components): add BulletList

---
+++ src/components/BulletList/index.tsx
@@ -0,0 +1,27 @@
+import { formatNonEmptyClassName } from "../../helpers";
+import { SeparatorInline } from "../Separators";
+
+import BulletListItem from "./Item";
+
+type BulletListProps = Omit<JSX.IntrinsicElements["ul"], "children"> & {
+  items: JSX.Element[];
+};
+
+export default function BulletList({ className, items, ...props }: BulletListProps) {
+  const Items = [<BulletListItem key={0}>{items[0]}</BulletListItem>];
+  let l = items.length;
+  for (let i = 1; i !== l; i++) {
+    const key = i * 2;
+    Items.push(<SeparatorInline key={key - 1}>•</SeparatorInline>);
+    Items.push(<BulletListItem key={key}>{items[i]}</BulletListItem>);
+  }
+
+  return (
+    <ul
+      className={`bullet-list d-flex flex-wrap p-0${formatNonEmptyClassName(className)}`}
+      {...props}
+    >
+      {Items}
+    </ul>
+  );
+}
