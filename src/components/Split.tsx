eb837070c0756e5ac8a5ad89967ddf12cd5359d7
TMK04 <floer.learner.01@gmail.com>
2023-03-21 21:51:42 +0800

    refactor(Split): make flex direction & gap responsive
    Flex Direction for print is still row

--- src/components/Split.tsx
+++ src/components/Split.tsx
@@ -9,7 +9,7 @@ export default function Split({
   article_children: JSX.Element;
 }) {
   return (
-    <div className="d-flex gap-5 split" {...props}>
+    <div className="d-flex flex-column flex-md-row gap-4 gap-md-5 split" {...props}>
       <aside className="d-flex flex-column gap-1 split-aside">{aside_children}</aside>
       <article className="d-flex flex-column gap-3 split-article">{article_children}</article>
     </div>
